88af166cd98f11328971e8794db4d95debc8801d
Chris Sinchok <csinchok@theonion.com>
2014-06-13 10:55:26 -0500

    New version, tracks multiple events, and has 404s

--- tinytracker/wsgi.py
+++ tinytracker/wsgi.py
@@ -1,6 +1,7 @@
 import base64
 import collections
 import os
+import sys
 import time
 import urlparse
 
@@ -10,7 +11,7 @@ from gevent import socket
 
 event_queue = queue.Queue()
 CARBON_ENDPOINT = os.environ.get("CARBON_ENDPOINT")
-INTERVAL = os.environ.get("INTERVAL", 10)
+INTERVAL = int(os.environ.get("INTERVAL", "10"))
 GIF_DATA = base64.b64decode("R0lGODlhAQABAIAAAP///////yH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==")
 
 
@@ -18,11 +19,13 @@ def send_events(message):
     if CARBON_ENDPOINT:
         sock = socket.create_connection((CARBON_ENDPOINT.split(":")))
         sock.sendall(message)
+        sock.close()
     else:
         print(message)
 
 
 def count_events():
+    
     while True:
         gevent.sleep(INTERVAL)
         counter = collections.Counter()
@@ -48,11 +51,15 @@ gevent.spawn(count_events)
 
 
 def application(env, start_response):
-    start_response("200 OK", [("Content-Type", "image/gif")])
-    yield GIF_DATA
+    if ["PATH_INFO"] == "/track.gif":
+        start_response("200 OK", [("Content-Type", "image/gif")])
+        yield GIF_DATA
 
-    parsed = urlparse.parse_qs(env["QUERY_STRING"])
+        parsed = urlparse.parse_qs(env["QUERY_STRING"])
 
-    events = parsed.get("event", [])
-    if events:
-        event_queue.put(events[0])
+        events = parsed.get("event", [])
+        for event in events:
+            event_queue.put(event)
+    else:
+        start_response("404 Not Found", [("Content-Type", "text/plain")])
+        yield "Nothing here."
